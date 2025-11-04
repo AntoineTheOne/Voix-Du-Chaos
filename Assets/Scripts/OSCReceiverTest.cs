341e13a68c505109e1e0019933f47b8e531fd5a9
AntoineTheOne <Antoine.Denis2@outlook.com>
2025-11-03 20:05:26 -0500

    ajout d'un timer comme condition d'apparition plus les ports différents

--- Assets/Scripts/OSCReceiverTest.cs
+++ Assets/Scripts/OSCReceiverTest.cs
@@ -4,14 +4,15 @@ using extOSC;
 public class OSCReceiverTest : MonoBehaviour
 {
     [SerializeField] private int port = 9000;
-    [SerializeField] private Transform spawner1;
-    [SerializeField] private Transform spawner2;
-    [SerializeField] private Transform spawner3;
-    [SerializeField] private Transform spawner4;
+    [SerializeField] private Transform spawner;
+   
     [SerializeField] private GameObject eau;
     [SerializeField] private GameObject feu;
     [SerializeField] private GameObject vent;
     [SerializeField] private GameObject terre;
+    [SerializeField] private float cooldown = 2f;
+
+    private float timer = 0f;
 
     private OSCReceiver receiver;
 
@@ -32,52 +33,72 @@ public class OSCReceiverTest : MonoBehaviour
         receiver.Bind("/854E4F5", OnCard_854E4F5);
         receiver.Bind("/88324E2", OnCard_88324E2);
 
-        
+
     }
 
-    
 
-    private bool CanTrigger(string cardCode)
+    void Update()
     {
-
-        if (lastCard == cardCode) return false;
-
-        lastCard = cardCode;
-        return true;
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
     }
+
+
 
     void OnCard_A19EBB5(OSCMessage message)
     {      
-        if (!CanTrigger("A19EBB5")) return;
-        Instantiate(terre, spawner2.position, spawner2.rotation);
-        Debug.Log("A19EBB5");
+      if(timer <= 0)
+        {
+            Instantiate(terre, spawner.position, spawner.rotation);
+            Debug.Log("A19EBB5");
+            timer = cooldown;
+        }
+        
     }
 
     void OnCard_9320076(OSCMessage message)
     {
-        if (!CanTrigger("9320076")) return;
-        Instantiate(eau, spawner1.position, spawner1.rotation);
-        Debug.Log("9320076");
+       if(timer <= 0)
+        {
+           Instantiate(eau, spawner.position, spawner.rotation);
+            Debug.Log("9320076"); 
+            timer = cooldown;
+        }
+        
     }
 
     void OnCard_8CEC54E(OSCMessage message)
     {
-        if (!CanTrigger("8CEC54E")) return;
-        Instantiate(feu, spawner2.position, spawner2.rotation);
-        Debug.Log("8CEC54E");
+        if(timer <= 0)
+        {
+            Instantiate(feu, spawner.position, spawner.rotation);
+            Debug.Log("8CEC54E");
+            timer = cooldown;
+        }
+        
     }
 
     void OnCard_854E4F5(OSCMessage message)
     {
-        if (!CanTrigger("854E4F5")) return;
-        Instantiate(terre, spawner3.position, spawner3.rotation);
-        Debug.Log("854E4F5");
+        if(timer <= 0)
+        {
+            Instantiate(terre, spawner.position, spawner.rotation);
+            Debug.Log("854E4F5");
+            timer = cooldown;
+        }
+        
     }
 
     void OnCard_88324E2(OSCMessage message)
     {
-        if (!CanTrigger("88324E2")) return;
-        Instantiate(vent, spawner4.position, spawner4.rotation);
-        Debug.Log("88324E2");
+        if(timer <= 0)
+        {
+            Instantiate(vent, spawner.position, spawner.rotation);
+            Debug.Log("88324E2");
+            timer = cooldown;
+        }
+        
     }
 }
