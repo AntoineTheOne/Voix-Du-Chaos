53172ee8fdc3af65a0a5f8715dafd46fbbfd9b70
Antoine Denis <Antoine.Denis2@outlook.com>
2025-11-10 16:22:41 -0500

    intégration de la scene prototypage jouabilité

--- Assets/Scripts/OSCReceiverTest.cs
+++ Assets/Scripts/OSCReceiverTest.cs
@@ -5,15 +5,12 @@ public class OSCReceiverTest : MonoBehaviour
 {
     [SerializeField] private int port = 9000;
     [SerializeField] private Transform spawner;
-   
-    [SerializeField] private GameObject eau;
-    [SerializeField] private GameObject feu;
-    [SerializeField] private GameObject vent;
-    [SerializeField] private GameObject terre;
+    [SerializeField] private GameObject PrefabAssassin;
+    [SerializeField] private GameObject PrefabGoblin; 
+    [SerializeField] private GameObject PrefabGolem;
+    [SerializeField] private GameObject PrefabMiniGolem;
     [SerializeField] private float cooldown = 2f;
-
     private float timer = 0f;
-
     private OSCReceiver receiver;
 
 
@@ -25,11 +22,11 @@ public class OSCReceiverTest : MonoBehaviour
 
         // Code de chaque carte RFID
 
-        receiver.Bind("/A19EBB5", OnCard_A19EBB5);
-        receiver.Bind("/9320076", OnCard_9320076);
-        receiver.Bind("/8CEC54E", OnCard_8CEC54E);
-        receiver.Bind("/854E4F5", OnCard_854E4F5);
-        receiver.Bind("/88324E2", OnCard_88324E2);
+        receiver.Bind("/A19EBB5", Assassin);
+        receiver.Bind("/9320076", Goblin);
+        receiver.Bind("/8CEC54E", Golem);
+        receiver.Bind("/854E4F5", MiniGolem);
+        
 
 
     }
@@ -45,58 +42,49 @@ public class OSCReceiverTest : MonoBehaviour
 
 
 
-    void OnCard_A19EBB5(OSCMessage message)
+    void Assassin(OSCMessage message)
     {      
       if(timer <= 0)
         {
-            Instantiate(terre, spawner.position, spawner.rotation);
+            Instantiate(PrefabAssassin, spawner.position, spawner.rotation);
             Debug.Log("A19EBB5");
             timer = cooldown;
         }
         
     }
 
-    void OnCard_9320076(OSCMessage message)
+    void Goblin(OSCMessage message)
     {
        if(timer <= 0)
         {
-           Instantiate(eau, spawner.position, spawner.rotation);
+           Instantiate(PrefabGoblin, spawner.position, spawner.rotation);
             Debug.Log("9320076"); 
             timer = cooldown;
         }
         
     }
 
-    void OnCard_8CEC54E(OSCMessage message)
+    void Golem(OSCMessage message)
     {
         if(timer <= 0)
         {
-            Instantiate(feu, spawner.position, spawner.rotation);
+            Instantiate(PrefabGolem, spawner.position, spawner.rotation);
             Debug.Log("8CEC54E");
             timer = cooldown;
         }
         
     }
 
-    void OnCard_854E4F5(OSCMessage message)
+    void MiniGolem(OSCMessage message)
     {
         if(timer <= 0)
         {
-            Instantiate(terre, spawner.position, spawner.rotation);
+            Instantiate(PrefabMiniGolem, spawner.position, spawner.rotation);
             Debug.Log("854E4F5");
             timer = cooldown;
         }
         
     }
 
-    void OnCard_88324E2(OSCMessage message)
-    {
-        if(timer <= 0)
-        {
-            Instantiate(vent, spawner.position, spawner.rotation);
-            Debug.Log("88324E2");
-            timer = cooldown;
-        }
-        
-    }
+    
 }
