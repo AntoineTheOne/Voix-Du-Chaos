179e707d0721a71f4d0a24bb3cccd8cdbcd39b60
Antoine <antdaoust@gmail.com>
2025-11-19 17:55:12 -0500

    Fin du cours

--- Assets/Skybox/SkyboxFade.cs
+++ Assets/Skybox/SkyboxFade.cs
@@ -1,35 +1,60 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SkyboxFade : MonoBehaviour
 {
-	
-	public Material material;
-	public Color[] colors;
-	private int currentColorIndex = 0;
-	private int targetColorIndex = 1;
-	private float targetPoint;
-	public float time;
-	
-	void Update()
-	{
-		Transition();
-	}
+    public Material material;
 
-	void Transition()
-	{
+    [Header("Shader Color Properties")]
+    public string sunDiscProperty = "_SunDiscColor";
+	public string sunHaloProperty = "_SunHaloColor";
+	public string horizonLineProperty = "_HorizonLineColor";
+    public string skyTopProperty = "_SkyGradientTop";
+    public string skyBottomProperty = "_SkyGradientBottom";
+
+    [Header("Transition Duration")]
+    public float duration = 5f;
+
+    void Start()
+    {
+        StartCoroutine(FadeSky());
+    }
+
+    IEnumerator FadeSky()
+    {
+        float t = 0f;
+
+        Color sunStart = new Color(1f, 1f, 1f);
+        Color sunEnd   = new Color(0.3396226f, 0.0227379f, 0.001601997f);
+
+		Color haloStart = new Color(1f, 1f, 1f);
+        Color haloEnd   = new Color(0.3396226f, 0.0227379f, 0.001601997f);
+
+		Color horizonStart = new Color(1f, 1f, 1f);
+        Color horizonEnd   = new Color(0.3396226f, 0.0227379f, 0.001601997f);
+
+        Color skyTopStart = new Color(1f, 0.5f, 0f);  // orange
+        Color skyTopEnd   = Color.red;               // red
 		
-		targetPoint += Time.deltaTime / time;
-		material.color = Color.Lerp(colors [currentColorIndex],colors [targetColorIndex], targetPoint);
-		if(targetPoint >= 1f)
-		{
-			targetPoint = 0f;
-			currentColorIndex = targetColorIndex;
-			targetColorIndex++;
-			if(targetColorIndex==colors.Length)
-				targetColorIndex = 0;
-		}
-		
-	}
+		Color skyBotStart = new Color(1f, 0.5f, 0f);  // orange
+        Color skyBotEnd   = Color.red;               // red
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime / duration;
+
+            // Lerp values
+            Color sunDisc = Color.Lerp(sunStart, sunEnd, t);
+            Color skyColTop  = Color.Lerp(skyTopStart, skyTopEnd, t);
+
+            // Apply to material
+            material.SetColor(sunDiscProperty, sunDisc);
+			material.SetColor(sunHaloProperty, sunHalo);
+			material.SetColor(horizonLineProperty, sunHorizon);
+            material.SetColor(skyTopProperty,  skyColTop);
+            material.SetColor(skyBottomProperty, skyColBot);
+
+            yield return null;
+        }
+    }
 }
