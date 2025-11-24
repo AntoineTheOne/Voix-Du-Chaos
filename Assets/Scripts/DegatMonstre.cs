af7765625f816ceea3d001240be35d718072cf2c
AntoineTheOne <Antoine.Denis2@outlook.com>
2025-11-24 10:02:38 -0500

    Modification du script lors des impacts avec le sol

--- Assets/Scripts/DegatMonstre.cs
+++ Assets/Scripts/DegatMonstre.cs
@@ -5,6 +5,9 @@ public class DegatMonstre : MonoBehaviour
     [SerializeField] private SpellsInfos spellsInfos; // Info sur le spell
     [SerializeField] private GameObject prefabExplosion; // prefab une fois la collision du sort avec un monstre pour donner un résultat
     private int nbDeDommage;
+    public LayerMask groundLayer;
+
+    [SerializeField] private bool hitOnImpact = false;
 
     void Start()
     {
@@ -16,6 +19,7 @@ public class DegatMonstre : MonoBehaviour
       if(prefabExplosion != null)
         {
             GameObject projectile = Instantiate(prefabExplosion, transform.position, transform.rotation);
+            
         }
       MonsterHpLoss vieEnnemi = collision.gameObject.GetComponent<MonsterHpLoss>();
       if (vieEnnemi != null)
@@ -23,7 +27,19 @@ public class DegatMonstre : MonoBehaviour
         vieEnnemi.PrendreDegats(nbDeDommage);
         Destroy(gameObject);
       }
-      Destroy(gameObject);
+
+      if (collision.gameObject.layer != LayerMask.NameToLayer("Ground"))
+        {
+          Destroy(gameObject);
+        }
+
+        if (hitOnImpact)
+        {
+            Destroy(gameObject);
+        }
+  
+    
+      
       
     }
 }
